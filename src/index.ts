173c3d24b85fce2dd589032d9357745e1729d088
teonateona12 <teona.piranishvili1@gmail.com>
2024-07-27 00:47:04 +0400

    feat: connect mongodb

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,20 @@
 import express, { Request, Response } from "express";
 import mongoose from "mongoose";
+
 const app = express();
+
+const mongoUri =
+  process.env.MONGO_URI ||
+  "mongodb+srv://teonapiranishvili1:teonateona12@cluster0.uimxgxn.mongodb.net/";
+
+mongoose
+  .connect(mongoUri)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error: Error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
 
 app.listen(5000, () => {
   console.log("Server is running on port 5000");

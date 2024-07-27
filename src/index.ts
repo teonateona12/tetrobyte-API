0235bf35d8bbb6cdc080be24cc292ea7dfd480e0
teonateona12 <teona.piranishvili1@gmail.com>
2024-07-27 11:05:32 +0400

    feat: create student routs

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 import express from "express";
 import connection from "./mongo.js";
+import cors from "cors";
+import router from "./routes/student.js";
 
 const app = express();
 
+app.use(cors());
 connection();
+app.use(express.json());
+app.use("/", router);
 
 app.listen(5000, () => {
   console.log("Server is running on port 5000");
---
+++ src/routes/student.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.get("/students", async (req, res) => {});
+
+router.post("/student", async (req, res) => {});
+
+router.delete("/student/:id", async (req, res) => {});
+
+router.put("/student/:id", async (req, res) => {});
+
+export default router;
